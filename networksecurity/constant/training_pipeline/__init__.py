5d5af0df87394a38a7534377c403b464da2360d2
Akilesh7kumar <akilesh7ramuk@gmail.com>
2026-01-11 14:02:08 +0530

    DataIngestion

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -11,6 +11,7 @@ ARTIFACT_DIR: str ="Artifacts"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 FILE_NAME: str = "phisingData.csv"
+SCHEMA_FILE_PATH = os.path.join('data_schema','schema.yaml')
 """
 DATA INGESTION REALTED CONSTANT START WITH DATA INGESTION NAME
 """
@@ -20,3 +21,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+DATA VALIDATION REALTED CONSTANT START WITH DATA INGESTION NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "Validated"
+DATA_VDALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VDALIDATION_DRIFT_REPORT_FILENAME: str = "report.yaml"
+
