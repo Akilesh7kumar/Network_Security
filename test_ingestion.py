ecff87566520326f82469b84fff3b42b2ab3b6a9
Akilesh7kumar <akilesh7ramuk@gmail.com>
2026-01-11 22:57:43 +0530

    DataValidation

--- test_ingestion.py
+++ test_ingestion.py
@@ -1,8 +1,10 @@
-from networksecurity.components import data_ingestion
+from networksecurity.components import data_ingestion,data_validation
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.logging.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
 import sys
 
 if __name__ == '__main__':
@@ -13,6 +15,13 @@ if __name__ == '__main__':
         logging.info('Initiating data ingestion')
         dataingestionartifct = data_ingestion.initiate_data_ingestion()
         print(dataingestionconfig)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(dataingestionartifct,data_validation_config)
+        logging.info("Initiate Data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info('data Validation Completed')
+        print(data_validation_artifact)
+
     except Exception as e: 
        raise NetworkSecurityException(error_message="Error occurred during data ingestion", error_details=str(e))
  
