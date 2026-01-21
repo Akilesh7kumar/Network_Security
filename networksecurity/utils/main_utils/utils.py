780587728d250560fdc9f7cd3d79ef6d5a46b088
Akilesh7kumar <akilesh7ramuk@gmail.com>
2026-01-21 21:58:10 +0530

    21-Jan

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import yaml
 from networksecurity.logging.logger import logging
 from networksecurity.logging.exception import NetworkSecurityException
@@ -61,3 +63,25 @@ def load_numpy_array(file_path:str):
             return np.load(file_obj)
     except Exception as e:
         raise NetworkSecurityException(e,sys) from e
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
--- test_ingestion.py
+++ test_ingestion.py
@@ -1,9 +1,10 @@
 from networksecurity.components import data_ingestion,data_validation
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -28,6 +29,13 @@ if __name__ == '__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_config)
         logging.info(data_transformation_artifact)
+        logging.info("model training started")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("model training artifact created")
+        
+        
 
     except Exception as e: 
        raise NetworkSecurityException(error_message="Error occurred during data ingestion", error_details=str(e))
